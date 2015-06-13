d586d7d87e40d90ccea9bab9a9d4a635c67b80df
Allen <vertago1@gmail.com>
2015-06-13 08:35:40 -0600

    added code to track location of hypens. added code to handle xml parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 
+//requires libboost-regex-dev okular-dev
+
 #include <cstdlib>
 
 #include <fstream>
@@ -6,6 +8,7 @@
 #include <sstream>
 
 #include <map>
+#include <set>
 #include <vector>
 
 #include <QtGui/QApplication>
@@ -51,7 +54,15 @@ namespace boost{
     }
 }
 
-regex_ns::basic_regex<std::string::iterator> regex_hypen_word = regex_ns::basic_regex<std::string::iterator>::compile("([A-Z]?[a-z]*)-$\n([a-z]+[,.?]?) ?");
+inline std::string qstr2str(const QString& qstr) {
+    return std::string(qstr.toAscii().constData());
+}
+
+inline QString str2qstr(const std::string& str) {
+    return QString(str.c_str());
+}
+
+regex_ns::basic_regex<std::string::iterator> regex_hypen_word = regex_ns::basic_regex<std::string::iterator>::compile("([A-Z]?[a-z]*)-$\n([a-z]+[,.?]?) ?|[^-](\n)");
 
 class page_record {
 public:
@@ -65,9 +76,18 @@ public:
         auto begin=page_text.begin();
         auto end=page_text.end();
         std::string result;
+        size_t line=0;
+        size_t col;
         while(regex_ns::regex_search(begin,end, sm, regex_hypen_word)){
+            ++line;
             result.append(sm.prefix());
-            result.append(*(sm.begin()+1)+*(sm.begin()+2)+"\n");
+            if (*(sm.begin()+3)=="\n"){
+                
+            } else {
+                col=sm.prefix().length()+(*(sm.begin()+1)).length()+1;
+                hypens.insert(std::make_pair(line,col));
+                result.append(*(sm.begin()+1)+*(sm.begin()+2)+"\n");
+            }
             begin = sm.suffix().first;
             //page_text.replace(sm.position()+(begin-page_text.begin()),(*sm.begin()).length(),*(sm.begin()+1)+*(sm.begin()+2)+"\n");
             //begin = sm.prefix().second;
@@ -79,6 +99,7 @@ public:
     std::string get_text() const { return page_text; }
     int get_lines() const { return lines; }
 private:
+    std::map<size_t,size_t> hypens;
     std::string page_text;
     int lines;
 };
@@ -172,13 +193,106 @@ int main(int argc, char **argv) {
     //write_sample_result(result);
     std::string result = get_sample_result();
     
+    enum : int {
+        flag_fromy               = 0x00000001,
+        flag_fromx               = 0x00000002,
+        flag_toy                 = 0x00000004,
+        flag_tox                 = 0x00000008,
+        flag_ruleid              = 0x00000010,
+        flag_msg                 = 0x00000020,
+        flag_replacements        = 0x00000040,
+        flag_context             = 0x00000080,
+        flag_contextoffset       = 0x00000100,
+        flag_offset              = 0x00000200,
+        flag_errorlength         = 0x00000400,
+        flag_category            = 0x00000800,
+        flag_locqualityissuetype = 0x00001000,
+    };
+    
+    std::map<std::string,int> attr_lookup({
+        std::make_pair("fromy",               flag_fromy),
+        std::make_pair("fromx",               flag_fromx),
+        std::make_pair("toy",                 flag_toy),
+        std::make_pair("tox",                 flag_tox),
+        std::make_pair("ruleid",              flag_ruleid),
+        std::make_pair("msg",                 flag_msg),
+        std::make_pair("replacements",        flag_replacements),
+        std::make_pair("context",             flag_context),
+        std::make_pair("contextoffset",       flag_contextoffset),
+        std::make_pair("offset",              flag_offset),
+        std::make_pair("errorlength",         flag_errorlength),
+        std::make_pair("category",            flag_category),
+        std::make_pair("locqualityissuetype", flag_locqualityissuetype),
+    });
+    
     QXmlStreamReader xml_parser(QString(result.c_str()));
     while (!xml_parser.atEnd()) {
         QXmlStreamReader::TokenType type=xml_parser.readNext();
         if(type==QXmlStreamReader::TokenType::StartElement && xml_parser.name()=="error"){
-            qDebug() << xml_parser.attributes().constBegin()->name();
+            size_t fromy, fromx, tox, toy, contextoffset, offset, errorlength;
+            std::string ruleid, msg, replacements, context, category, locqualityissuetype;
+            uint32_t flags=0;
+            for(const auto attribute : xml_parser.attributes()){
+                qDebug() << attribute.name();
+                auto itr = attr_lookup.find(qstr2str(*attribute.name().string()));
+                if(itr!=attr_lookup.end()){
+                    switch(itr->second){
+                        case flag_fromy:
+                            fromy=std::stoi(qstr2str(*attribute.value().string()));
+                            flags|=flag_fromy;
+                            break;
+                        case flag_fromx:
+                            fromx=std::stoi(qstr2str(*attribute.value().string()));
+                            flags|=flag_fromx;
+                            break;
+                        case flag_toy:
+                            toy=std::stoi(qstr2str(*attribute.value().string()));
+                            flags|=flag_toy;
+                            break;
+                        case flag_tox:
+                            tox=std::stoi(qstr2str(*attribute.value().string()));
+                            flags|=flag_tox;
+                            break;
+                        case flag_ruleid:
+                            ruleid=qstr2str(*attribute.value().string());
+                            flags|=flag_ruleid;
+                            break;
+                        case flag_msg:
+                            msg=qstr2str(*attribute.value().string());
+                            flags|=flag_msg;
+                            break;
+                        case flag_replacements:
+                            replacements=qstr2str(*attribute.value().string());
+                            flags|=flag_replacements;
+                            break;
+                        case flag_context:
+                            context=qstr2str(*attribute.value().string());
+                            flags|=flag_context;
+                            break;
+                        case flag_contextoffset:
+                            contextoffset=std::stoi(qstr2str(*attribute.value().string()));
+                            flags|=flag_contextoffset;
+                            break;
+                        case flag_offset:
+                            offset=std::stoi(qstr2str(*attribute.value().string()));
+                            flags|=flag_offset;
+                            break;
+                        case flag_errorlength:
+                            errorlength=std::stoi(qstr2str(*attribute.value().string()));
+                            flags|=flag_errorlength;
+                            break;
+                        case flag_category: //std::make_pair("category",            12),
+                            category=qstr2str(*attribute.value().string());
+                            flags|=flag_category;
+                            break;
+                        case flag_locqualityissuetype: //std::make_pair("locqualityissuetype", 13),
+                            locqualityissuetype=qstr2str(*attribute.value().string());
+                            flags|=flag_locqualityissuetype;
+                            break;
+                    }
+                }
+            }
         }
-        
     }
     
 
